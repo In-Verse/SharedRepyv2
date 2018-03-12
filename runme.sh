d0b0128b6d15efee5e0adcaad6022192038bdc7b
In-verse <dayaftertomorrow.davies@gmail.com>
2018-03-12 13:29:08 -0700

    Fixed runme.sh to test for all the defense files: 1-13

--- runme.sh
+++ runme.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "hereissomedata" > look.txt
-python repy.py restrictions.default encasementlib.r2py my_defense.r2py attack.r2py
+for i in {1..13}
+do
+	file=$i
+    # Start each test with a fresh amount of data in look.txt
+    echo "-"
+	echo "I am running file$i.r2py"
+	echo "-"
+	echo "hereissomedata" > look.txt
+    python repy.py restrictions.default encasementlib.r2py file$i.r2py attack.r2py
+done
